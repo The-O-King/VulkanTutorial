6d05c8c0251aef7ee5dbb94f803cb9878cc0ebd2
TheOKing <techmanozmar@gmail.com>
2018-01-10 18:55:34 -0500

    Implemented an Index Buffer to allow for the reuse of vertices

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -58,9 +58,14 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-  {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-  {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-  {{-0.5f, 0.75f}, {0.0f, 0.0f, 1.0f}},
+  {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+  {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+  {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+  {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+};
+
+const std::vector<uint16_t> indices = {
+  0, 1, 2, 2, 3, 0
 };
 
 
@@ -152,11 +157,13 @@ private:
   // Synchronization Semaphores for pulling from the swapchain
   VkSemaphore imageAvailableSemaphore;
   VkSemaphore renderFinishedSemaphore;
-  // Vertex Buffer that we will pass to shaders
+  // Vertex Buffer and associated memory that we will pass to shaders
   VkBuffer vertexBuffer;
-  // The Vertex Buffer's allocated memory
   VkDeviceMemory vertexBufferMemory;
-
+  // Index Buffer and associated memory to resuse vertices
+  VkBuffer indexBuffer;
+  VkDeviceMemory indexBufferMemory;
+  
   
   // Function encapsulating the code needed to create a window using GLFW
   void initWindow(){
@@ -197,6 +204,7 @@ private:
     createFramebuffers();
     createCommandPool();
     createVertexBuffer();
+    createIndexBuffer();
     createCommandBuffers();
     createSemaphores();
   }
@@ -214,6 +222,8 @@ private:
   
   void cleanup() {
     cleanupSwapChain();
+    vkDestroyBuffer(device, indexBuffer, nullptr);
+    vkFreeMemory(device, indexBufferMemory, nullptr);
     vkDestroyBuffer(device, vertexBuffer, nullptr);
     vkFreeMemory(device, vertexBufferMemory, nullptr);
     vkDestroySemaphore(device, renderFinishedSemaphore, nullptr);
@@ -751,7 +761,7 @@ private:
 
     VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
     inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
+    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
     inputAssembly.primitiveRestartEnable = VK_FALSE;
 
     VkViewport viewport = {};
@@ -1051,7 +1061,28 @@ private:
     vkFreeMemory(device, stagingBufferMemory, nullptr);
   }
 
-  
+  ///////////////////////// Index Buffer Creation /////////////////////////////////////////
+
+  void createIndexBuffer(){
+    VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), (size_t) bufferSize);
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+    copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+    
+  }
+
   /////////////////////// Command Pool and Buffers Creation /////////////////////////////////
 
   void createCommandPool(){
@@ -1105,7 +1136,8 @@ private:
       VkBuffer vertexBuffers[] = {vertexBuffer};
       VkDeviceSize offsets[] = {0};
       vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-      vkCmdDraw(commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+      vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+      vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
       vkCmdEndRenderPass(commandBuffers[i]);
 
       if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS){
